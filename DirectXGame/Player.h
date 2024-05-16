bfc389a86b9de607c48f576c672f87aa9c6e76dd
na <k023g1066@g.neec.ac.jp>
2024-05-16 15:57:40 +0900

    player.h

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -2,15 +2,22 @@
 #pragma once
 #include "WorldTransform.h"
 #include "Model.h"
+#include "ViewProjection.h"
 
 class Player
 {
 public:
+	///コンストラクタ
+	Player();
+
+	///デストラクタ
+	~Player();
+
 	/// <summary>
 	/// 初期化
 	/// モデルのポインタ
 	/// </summary>
-	void Initialize(Model* model, uint32_t textureHandle);
+	void Initialize(Model* model, uint32_t textureHandle, ViewProjection* viewProjection);
 	/// <summary>
 	/// 更新処理
 	/// </summary>
@@ -27,5 +34,6 @@ private:
 	Model* model_ = nullptr;
 	//テクスチャハンドル
 	uint32_t textureHandle_ = 0u;
-
+	//ビュープロジェクション
+	ViewProjection* viewProjection_ = nullptr;
 };

aa9efe57961cfb74254ab237df32995dcb22dab9
na <k023g1066@g.neec.ac.jp>
2024-07-01 15:31:06 +0900

    敵の判定ほっぽって、パーティクル

---
+++ DirectXGame/DeathParticles.cpp
@@ -0,0 +1,16 @@
+#include "DeathParticles.h"
+
+//void DeathParticles::Initialize(Model* model, ViewProjection* viewProjection, const Vector3& position)
+//{
+//
+//}
+//
+//void DeathParticles::Update()
+//{
+//
+//}
+//
+//void DeathParticles::Draw()
+//{
+//
+//}
---
+++ DirectXGame/DeathParticles.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include"Vector3.h"
+
+class Model;
+class ViewProjection;
+class DeathParticles;
+
+/// <summary>
+/// デス演出用パーティクル
+/// </summary>
+class DeathParticles
+{
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize(Model* model, ViewProjection* viewProjection, const Vector3& position);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+	/// <summary>
+	///描画 
+	/// </summary>
+	void Draw();
+	/// <summary>
+	/// 
+	/// </summary>
+private:
+	Model* model_;
+	ViewProjection* viewProjection_;
+};
